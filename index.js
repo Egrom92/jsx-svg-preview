38c64f277729dc5f470baf7b52dcfe52a1dc6384
Egrom92 <mr.roman.egliens@gmail.com>
2023-02-23 23:09:35 +0200

    made it possible to preview the icons

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-#!/usr/bin/env node
-
+const argv = require('minimist')(process.argv.slice(2));
+console.log(argv.p);
+const svgFolder = argv.p;
 const fs = require('fs').promises;
 const path = require('path');
-var argv = require('minimist')(process.argv.slice(2));
-const svgFolder = argv.p;
 
-const useImportTemplate = (fileName) => {
+const useImportTemplateSnippet = (fileName) => {
     return `import ${fileName} from './${fileName}.jsx';\n`;
 };
 
@@ -13,21 +12,37 @@ const useExportTemplateSnippet = (fileName) => {
     return `    ${fileName},\n`;
 };
 
+const useSvgPreviewSnipet = (fileName) => {
+    return `            <p>${fileName}:</p> <br/><${fileName}/> <br/> <br/> <br/> <br/>\n`;
+};
+
 fs.readdir(svgFolder)
     .then((files) => {
         const JSXFiles = [];
         let imports = '';
+        let previews = [];
 
         files.forEach((file) => {
             if (path.extname(file) === '.jsx') {
-                imports += useImportTemplate(path.parse(file).name);
-                JSXFiles.push(useExportTemplateSnippet(path.parse(file).name));
+                const fileName = path.parse(file).name;
+
+                imports += useImportTemplateSnippet(fileName);
+                JSXFiles.push(useExportTemplateSnippet(fileName));
+                previews.push(useSvgPreviewSnipet(fileName));
             }
         });
 
-        const exportSnippet = `export {\n${JSXFiles.join('')}};\n`;
+        const previewsSnipet = `\nconst SVGPreview = () => {\n    return (\n        <div style={{padding: '60px'}}>\n${previews.join(
+            ''
+        )}\n        </div>\n    )\n}\n`;
+        const exportSnippet = `\nexport {\n    SVGPreview,\n${JSXFiles.join(
+            ''
+        )}};\n`;
 
-        return fs.writeFile('mynewfile.js', imports + exportSnippet);
+        return fs.writeFile(
+            './src/svg/index.js',
+            imports + previewsSnipet + exportSnippet
+        );
     })
     .then(() => {
         console.log('Updated!');
