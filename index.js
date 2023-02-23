b170a982c993405c8f8ea5b284a4d161277b7ab3
Egrom92 <mr.roman.egliens@gmail.com>
2023-02-24 01:58:08 +0200

    I added a search for a swg file, change the format, and remake the file into a component.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const argv = require('minimist')(process.argv.slice(2));
-console.log(argv.p);
-const svgFolder = argv.p;
+// const argv = require('minimist')(process.argv.slice(2));
+// console.log(argv.p)
+// const svgFolder = argv.p;
 const fs = require('fs').promises;
 const path = require('path');
 
@@ -16,19 +16,52 @@ const useSvgPreviewSnipet = (fileName) => {
     return `            <p>${fileName}:</p> <br/><${fileName}/> <br/> <br/> <br/> <br/>\n`;
 };
 
-fs.readdir(svgFolder)
+function transformFileName(str) {
+    const parts = str.split(/(?=[A-Z])/); // Split the string by capital letters
+    const newParts = parts.map((part) => part.toLowerCase()); // Convert each part to lowercase
+    return newParts.join('-'); // Join the parts with hyphens
+}
+
+fs.readdir('./src/svg')
     .then((files) => {
         const JSXFiles = [];
         let imports = '';
         let previews = [];
 
         files.forEach((file) => {
-            if (path.extname(file) === '.jsx') {
-                const fileName = path.parse(file).name;
+            const fileName = path.parse(file).name;
 
+            if (path.extname(file) === '.jsx') {
                 imports += useImportTemplateSnippet(fileName);
                 JSXFiles.push(useExportTemplateSnippet(fileName));
                 previews.push(useSvgPreviewSnipet(fileName));
+            } else if (path.extname(file) === '.svg') {
+                const oldPath = path.join('./src/svg', file);
+                const newPath = path.join('./src/svg', `${fileName}.jsx`);
+
+                try {
+                    fs.rename(oldPath, newPath);
+                    imports += useImportTemplateSnippet(fileName);
+                    JSXFiles.push(useExportTemplateSnippet(fileName));
+                    previews.push(useSvgPreviewSnipet(fileName));
+                    console.log('File renamed successfully');
+
+                    fs.readFile(newPath, 'utf8').then((data) => {
+                        const svgString = data.toString();
+                        const newSvgString = svgString.replace(
+                            /<svg/g,
+                            `<svg className={classes}`
+                        );
+                        fs.writeFile(
+                            newPath,
+                            `const ${fileName} = (props) => {\n    const { color = '', className } = props;\n\n    const classes = \`${transformFileName(
+                                fileName
+                            )}\${className ? ' ' + className : ''}\`;\n\n    return (\n        ${newSvgString}\n    );\n};\n\nexport default ${fileName};`
+                        );
+                    });
+                } catch (err) {
+                    console.error(err);
+                }
             }
         });
 
